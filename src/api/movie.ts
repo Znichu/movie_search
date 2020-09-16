0ac2c9a58ba27eca866b5cc756bbeb82b73c784b
Sergey Neplashov <znichu1989@gmail.com>
2020-09-16 12:54:35 +0300

    fix bug api

--- src/api/movie.ts
+++ src/api/movie.ts
@@ -1,13 +1,14 @@
 import axios from "axios";
 
 export const instance = axios.create({
+    withCredentials: true,
     baseURL: "https://api.themoviedb.org/3/"
 });
 const api_key = process.env.REACT_APP_API_KEY;
 
 export const movieAPI = {
     getPopularMovies(currentPage: number = 1) {
-        return instance.get(`movie/popular?api_key=${api_key}=en-US&page=${currentPage}`)
+        return instance.get(`movie/popular?api_key=${api_key}&language=en-US&page=${currentPage}`)
             .then((response) => response.data);
     }
 };
