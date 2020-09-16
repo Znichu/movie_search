b9826c2c979ced11dba2faa4050ffbaf753640c0
Sergey Neplashov <znichu1989@gmail.com>
2020-09-16 16:50:35 +0300

    style

--- src/components/MoviePage/MoviePage.tsx
+++ src/components/MoviePage/MoviePage.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import style from "./MoviePage.module.scss"
 
 export const MoviePage = () => {
     return (
-        <div>
-MOVIE PAGE
+        <div className={style.movies}>
+            <div className={style.container}>
+                <div className={style.movies__block}>
+                    <div className={style.title}>
+                        <h2>Movies</h2>
+                    </div>
+                    <div className={style.tabs}></div>
+                </div>
+            </div>
         </div>
     )
 }
