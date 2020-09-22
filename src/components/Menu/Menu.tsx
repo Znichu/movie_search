f33fb850ed694ab9291b2e64f10fd3be4557abd1
Sergey Neplashov <znichu1989@gmail.com>
2020-09-22 16:24:43 +0300

    refactoring

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -5,9 +5,9 @@ export const Menu = () => {
     return (
         <main >
             <nav className={style.tab__link}>
-                <a>home</a>
-                <a>shop</a>
-                <a>blog</a>
+                <a>#Popular</a>
+                <a>#Top rated</a>
+                <a>#Upcoming</a>
                 <a>about</a>
                 <a>contact</a>
                 <hr/>
