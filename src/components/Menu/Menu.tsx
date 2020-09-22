a7ed69aef5e45077e01ac01c6c7fc47629bedb4e
Znichu <sergeyznich@gmail.com>
2020-09-23 00:29:59 +0300

    styling menu component

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import style from "./Menu.module.scss"
-import {NavLink} from "react-router-dom";
+import {Link, NavLink, useLocation} from "react-router-dom";
 
 export const Menu = () => {
+
+    let location = useLocation();
+
     return (
-        <main >
-            <nav className={style.tab__link}>
-                <NavLink to={'/'} >#Popular</NavLink>
-                <NavLink to={'/top-rated'}>#Top rated</NavLink>
-                <NavLink to={'/upcoming'}>#Upcoming</NavLink>
-                <a>about</a>
-                <a>contact</a>
-                <hr/>
-            </nav>
-        </main>
+        <div className={style.wrapper}>
+            <ul className={style.snip}>
+                <li className={location.pathname === '/' ? style.current : ""}>
+                    <Link to={"/"} data-hover="Popular">Popular</Link>
+                </li>
+                <li className={location.pathname === '/top-rated' ? style.current : ""}>
+                    <Link to={"/top-rated"} data-hover="Top rated">Top rated</Link>
+                </li>
+                <li className={location.pathname === '/upcoming' ? style.current : ""}>
+                    <Link to={"/upcoming"} data-hover="Upcoming">Upcoming</Link>
+                </li>
+                <li><a href="#" data-hover="Services">Services</a></li>
+                <li><a href="#" data-hover="Products">Products</a></li>
+                <li><a href="#" data-hover="Contact">Contact</a></li>
+            </ul>
+        </div>
     )
 }
